5f2b4ae8b26209be6fcb3c5ddbd81d0f1534a1cd
Kop1tes <prsasha0@gmail.com>
2022-11-10 14:15:35 +0200

    add third change

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,11 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => { // обов'язково передаєм event параметром
+    event.preventDefault(); //недаємо перезавантажити сторінку
+    const {elements: { email, password }} = event.currentTarget; //отримуємо значення полів форми
+    if (email.value === "" || password.value === "") { //перевіряємо чи є пусті поля
+      alert("Заповніть всі поля обов'язково!!!");
+    } else {
+    console.log({ "email": email.value, "password": password.value });//виводимо об'єкт в консоль
+      form.reset();//скидаємо форму
+    }
+  });
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+let colection = [];
+
+input.addEventListener('click', (event) => {
+  const value = event.currentTarget.value;
+  console.log(value);
+  return value;
+});
+
+createBtn.addEventListener('click', () => {
+  colection.push(value);
+});
+
+console.log(colection);
+
+// не получается решить
